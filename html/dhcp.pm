a7f63f04d870008635afb75b0ae52028244885fc
ClaudiuSchuster <info@x0e.de>
2018-07-08 17:09:29 +0000

    html/dhcp.pm: DHCP-Frontent use now 'POSTDATA' and small improvements

--- html/dhcp.pm
+++ html/dhcp.pm
@@ -10,18 +10,34 @@ use methods::dhcp;
 
 sub print {
     my $cgi = shift;
-    my $method_dhcp = API::methods::dhcp::run($cgi,{ meta => { rc => 200, msg => undef, method => undef, json => undef}, data => {} });
+    
+    ### Initialize JSON structure and execute API::methods::dhcp::run
+    my $json = {
+        meta => {
+            rc => 200,
+            msg => undef,
+            method => undef,
+            postdata => undef
+        },
+        data => {}
+    };
+    eval { $json->{meta}{postdata} = decode_json( $cgi->param('POSTDATA') || "{}" ); 1; } or do { 
+        $json->{meta}{rc}  = 400;
+        $json->{meta}{msg} = 'error.decode_json: '.$@;
+    };
+    my $method_dhcp = API::methods::dhcp::run($cgi,$json);
         die  "Error! Try JSON API for detailed error.\n" unless $method_dhcp;
+        
     my $dhcpd = $method_dhcp->{data};
     my $generateDhcpdConf_sub = $method_dhcp->{generateDhcpdConf_sub};
     my $sitePath = $cgi->path_info();
+    
     print $cgi->header, $cgi->start_html(
         -title=>"pxe.mine.io",
         -style=>{-code=>"body {background-color: #0B2F3A; color: #81BEF7; font-family: Arial, Helvetica, sans-serif; font-size: 17px;} 
                         a {color: #A9F5A9; text-decoration: none;} 
                         a:hover {text-decoration: underline;}"}
     );
-    
     print "<div style='padding-bottom: 4px; color: #2EFEF7;'>service isc-dhcp-server: "
          .(${$dhcpd->{status}{active}} ? '<span style="color: lightgreen;">running</span>' : '<span style="color: red;">stopped</span>')." - since: ".$dhcpd->{status}{lstart}." (".$dhcpd->{status}{etimes}." sec ago)</div>";
     print "<div>";
@@ -29,10 +45,18 @@ sub print {
                         type="button" value="-- Reload --" onclick="window.location.href='http://$ENV{HTTP_HOST}$sitePath'" /></form> ~;
     print qq~<form><input style="margin-right: 20px; width: 220px; background-color: #a163ff; color: white; font-weight: bold; float: left;" 
                         type="button" value="-- API Dokumentation --" onclick="window.location.href='http://$ENV{HTTP_HOST}/readme'" /></form> ~;
-    print qq~<form><input style="margin-right: 20px; width: 220px; background-color: lightblue; color: darkblue; font-weight: bold; float: left;" 
-                        type="button" value="-- Get JSON --" onclick="window.location.href='http://$ENV{HTTP_HOST}?method=dhcp'" /></form> ~;
-    print qq~<form><input style="width: 220px; background-color: #F78181; color: #0B2F3A; font-weight: bold; float: left;" 
-                        type="button" value="! Restart ISC-DHCP-SERVER !" onclick="window.location.href='http://$ENV{HTTP_HOST}$sitePath?method=dhcp.service.restart'" /></form> ~;
+    print qq~<form method="post" action="#showConf" enctype='text/plain'>
+                <input type="hidden" name='{"generateDhcpdConf":1, "htmlFormTrash":"' value='"}'>
+                <input style="margin-right: 20px; width: 220px; background-color: #ccccff; color: #333300; font-weight: bold; float: left;" type="submit" value="-- Show dhcpd.conf --"  />
+             </form> ~;
+    print qq~<form method="post" action="/" enctype='text/plain'>
+                <input type="hidden" name='{"method":"dhcp", "htmlFormTrash":"' value='"}'>
+                <input style="margin-right: 20px; width: 220px; background-color: lightblue; color: darkblue; font-weight: bold; float: left;" type="submit" value="-- Get JSON --" />
+             </form> ~;
+    print qq~<form method="post" enctype='text/plain'>
+                <input type="hidden" name='{"method":"dhcp.service.restart", "htmlFormTrash":"' value='"}'>
+                <input style="width: 220px; background-color: #F78181; color: #0B2F3A; font-weight: bold; float: left;" type="submit" value="! Restart ISC-DHCP-SERVER !"  />
+             </form> ~;
     print "</div><br/><br/>";
     for my $groupName (sort keys %{$dhcpd->{groups}}) {
         if ( $groupName =~ /winbios|.*-dev/ && keys %{$dhcpd->{groups}{$groupName}{hosts}} > 0 && ( !grep{$_ =~ /pxe.test.*/i} keys %{$dhcpd->{groups}{$groupName}{hosts}} or keys %{$dhcpd->{groups}{$groupName}{hosts}} > 1 ) ) {
@@ -49,13 +73,8 @@ sub print {
                     for my $inGroupName (sort keys %{$dhcpd->{groups}}) {
                         print qq~
                         <span style='float:left; margin-left: 5px; overflow: hidden;'>
-                        <form method="post" id="move_$hostName$inGroupName">
-                            <input type="hidden" name="method" value="dhcp.host.alter">
-                            <textarea name="json" style="display:none;">
-                        ~;
-                        print encode_json( {group => $inGroupName, name => $hostName} );
-                        print qq~ 
-                            </textarea>
+                        <form method="post" enctype='text/plain' id="move_$hostName$inGroupName">
+                            <input type="hidden" name='{"method":"dhcp.host.alter","params":{"group":"$inGroupName","name":"$hostName"}, "htmlFormTrash":"' value='"}'>
                             <a href="javascript:{}" onclick="document.getElementById('move_$hostName$inGroupName').submit(); return false;">&raquo; $inGroupName</a>
                         </form></span>
                         ~;
@@ -64,13 +83,8 @@ sub print {
                 if($groupName !~ /nvidia.*/i) { 
                     print qq~
                     <span style='float:left; margin-left: 5px; overflow: hidden;'>
-                    <form method="post" id="move_winbios_$hostName">
-                        <input type="hidden" name="method" value="dhcp.host.alter">
-                        <textarea name="json" style="display:none;">
-                    ~;
-                    print encode_json( {group => 'winbios', name => $hostName} );
-                    print qq~ 
-                        </textarea>
+                    <form method="post" enctype='text/plain' id="move_winbios_$hostName">
+                        <input type="hidden" name='{"method":"dhcp.host.alter","params":{"group":"winbios","name":"$hostName"}, "htmlFormTrash":"' value='"}'>
                         <a href="javascript:{}" onclick="document.getElementById('move_winbios_$hostName').submit(); return false;">&raquo; winbios</a>
                     </form></span>
                     ~;
@@ -78,13 +92,8 @@ sub print {
                 if($groupName =~ /nvidia.*/i) {  ## nvidia-dev
                     print qq~
                     <span style='float:left; margin-left: 5px; overflow: hidden;'>
-                    <form method="post" id="move_nvidia-dev_$hostName">
-                        <input type="hidden" name="method" value="dhcp.host.alter">
-                        <textarea name="json" style="display:none;">
-                    ~;
-                    print encode_json( {group => 'nvidia-dev', name => $hostName} );
-                    print qq~ 
-                        </textarea>
+                    <form method="post" enctype='text/plain' id="move_nvidia-dev_$hostName">
+                        <input type="hidden" name='{"method":"dhcp.host.alter","params":{"group":"nvidia-dev","name":"$hostName"}, "htmlFormTrash":"' value='"}'>
                         <a href="javascript:{}" onclick="document.getElementById('move_nvidia-dev_$hostName').submit(); return false;">&raquo; nvidia-dev</a>
                     </form></span>
                     ~;
@@ -92,13 +101,8 @@ sub print {
                 if($groupName =~ /amd.*/i) {  ## amd-dev
                     print qq~
                     <span style='float:left; margin-left: 5px; overflow: hidden;'>
-                    <form method="post" id="move_amd-dev_$hostName">
-                        <input type="hidden" name="method" value="dhcp.host.alter">
-                        <textarea name="json" style="display:none;">
-                    ~;
-                    print encode_json( {group => 'amd-dev', name => $hostName} );
-                    print qq~ 
-                        </textarea>
+                    <form method="post" enctype='text/plain' id="move_amd-dev_$hostName">
+                        <input type="hidden" name='{"method":"dhcp.host.alter","params":{"group":"amd-dev","name":"$hostName"}, "htmlFormTrash":"' value='"}'>
                         <a href="javascript:{}" onclick="document.getElementById('move_amd-dev_$hostName').submit(); return false;">&raquo; amd-dev</a>
                     </form></span>
                     ~;
@@ -106,13 +110,8 @@ sub print {
             } else {  ## back to vivso group (original group)
                 print qq~
                 <span style='float:left; margin-left: 5px; overflow: hidden;'>
-                <form method="post" id="move_vivso_$hostName">
-                    <input type="hidden" name="method" value="dhcp.host.alter">
-                    <textarea name="json" style="display:none;">
-                ~;
-                print encode_json( {group => $dhcpd->{groups}{$groupName}{hosts}{$hostName}{vivso}, name => $hostName} );
-                print qq~ 
-                    </textarea>
+                <form method="post" enctype='text/plain' id="move_vivso_$hostName">
+                    <input type="hidden" name='{"method":"dhcp.host.alter","params":{"group":"$dhcpd->{groups}{$groupName}{hosts}{$hostName}{vivso}","name":"$hostName"}, "htmlFormTrash":"' value='"}'>
                     <a style="color: #FA58F4;" href="javascript:{}" onclick="document.getElementById('move_vivso_$hostName').submit(); return false;">&raquo; $dhcpd->{groups}{$groupName}{hosts}{$hostName}{vivso}</a>
                 </form></span>
                 ~;
@@ -120,9 +119,8 @@ sub print {
             print "</div>";
         }
     }
-    print qq~<br/><br/><form><input style="float: left;" type="button" value="Show dhcpd.conf" onclick="window.location.href='http://$ENV{HTTP_HOST}$sitePath?generate=1'" /></form><br/><br/> ~;
-    if($cgi->param('generate')) {
-        print "<br/><pre style='font-size: 12px;'>", $cgi->escapeHTML( $generateDhcpdConf_sub->() ), "</pre>\n";
+    if( $json->{meta}{postdata}{generateDhcpdConf} ) {
+        print "<br/><hr/><pre style='font-size: 12px;' id='showConf'>", $cgi->escapeHTML( $generateDhcpdConf_sub->() ), "</pre>\n";
     }
     print $cgi->end_html;
 }
