5942644b4c7358ff2af37e024d7a93e4fb204e52
ClaudiuSchuster <info@x0e.de>
2018-07-11 17:33:01 +0000

    html/dhcp.pm: Add data inplace.

--- methods/dhcp.pm
+++ methods/dhcp.pm
@@ -2,86 +2,25 @@ package API::methods::dhcp;
 
 use strict; use warnings; use utf8; use feature ':5.10';
 
+use JSON;
 ## Load Net::ISC::DHCPd 0.1709
 use Net::ISC::DHCPd::Config;
 use Net::ISC::DHCPd::Leases;
-## Load some more modules
-use JSON;
 ## Load our dhcp modules...
 use methods::dhcp::service;
 use methods::dhcp::group;
 use methods::dhcp::host;
 
 
-### Initialize dhcp paths...
-our $dhcpdPath = '/usr/sbin/dhcpd';
-our $filename = '/etc/dhcp/dhcpd.conf';
-our $leasefile = '/var/lib/dhcp/dhcpd.leases';
-
-
-sub create_dhcp_hash { ### Create %dhcpd hash with all infos from DHCPD
-    my $json = shift;
-    my $config = shift;
-    my $leases = shift;
-    
-    my %dhcpd;
-    my $pid = API::helpers::trim(`pidof $dhcpdPath`) || 0;
-    $dhcpd{status} = {
-        active => $pid ? \1 : \0,
-        lstart => $pid ? API::helpers::trim(`ps -o lstart= -p $pid`) : undef,
-        etimes => $pid ? API::helpers::trim(`ps -o etimes= -p $pid`) : undef,
-    };
-    for my $group ( $config->find_groups({}) ) {
-        for ( $group->options ) {
-            $dhcpd{'groups'}{$group->{name}}{'options'}{$_->{name}} = $_->{value};
-        }
-        for my $host ( @{$group->hosts} ) {
-            my $mac = $host->_children->[0]->{value};
-            my ($lease) = grep { $_->hardware_address =~ /$mac/i } @{$leases->leases};
-            $dhcpd{'groups'}{$group->{name}}{'hosts'}{$host->{name}} = {
-                name             => $host->{name},
-                hardware_address => $host->_children->[0]->{value},
-                vivso            => $host->options->[0]->{value},
-            };
-            $dhcpd{'groups'}{$group->{name}}{'hosts'}{$host->{name}}{'lease'} = {
-               ip_address => $lease->ip_address, client_hostname => $lease->client_hostname,
-               state => $lease->state, starts => $lease->starts, ends => $lease->ends,
-            } if( defined $lease );
-        }
-        $dhcpd{'groups'}{$group->{name}} = {} unless defined($dhcpd{'groups'}{$group->{name}});
-    }
-    for my $lease ( $leases->leases ) {
-        $dhcpd{'leases'}{$lease->ip_address}{'hardware_address'} = $lease->hardware_address;
-        $dhcpd{'leases'}{$lease->ip_address}{'client_hostname'}  = $lease->client_hostname;
-        $dhcpd{'leases'}{$lease->ip_address}{'starts'}           = $lease->starts;
-        $dhcpd{'leases'}{$lease->ip_address}{'state'}            = $lease->state;
-        $dhcpd{'leases'}{$lease->ip_address}{'ends'}             = $lease->ends;
-        
-        my $mac = $lease->hardware_address;
-        if( defined $mac ) {
-            for my $groupName (sort keys %{$dhcpd{groups}}) {
-                if( defined $dhcpd{groups}{$groupName}{hosts} ) {
-                    my ($host) = grep { $dhcpd{groups}{$groupName}{hosts}{$_}->{hardware_address} =~ /$mac/i } keys %{$dhcpd{groups}{$groupName}{hosts}};
-                    if( $host ) {
-                        $dhcpd{'leases'}{$lease->ip_address}{'host'}             = {
-                            name  => $dhcpd{groups}{$groupName}{hosts}{$host}{name},
-                            vivso => $dhcpd{groups}{$groupName}{hosts}{$host}{vivso},
-                            group => $groupName,
-                        };
-                        last; 
-                    }
-                }
-            }
-        }
-    }
-    
-    $json->{data}{dhcp} = \%dhcpd;
-}
-
 sub run {
     my $cgi = shift;
     my $json = shift;
     my $isHtml = shift;
+    
+    ### Initialize dhcp paths...
+    my $dhcpdPath = '/usr/sbin/dhcpd';
+    my $filename = '/etc/dhcp/dhcpd.conf';
+    my $leasefile = '/var/lib/dhcp/dhcpd.leases';
     
     ### Parsing dhcpd.conf and leases
     my ($config, $leases);
@@ -117,6 +56,58 @@ sub run {
         }
         close (OUT);
     };
+    ### sub: Create %dhcpd hash with all infos from DHCPD
+    my $create_dhcp_hash = sub {
+        my $pid = API::helpers::trim(`pidof $dhcpdPath`) || 0;
+        $json->{data}{dhcp}{status} = {
+            active => $pid ? \1 : \0,
+            lstart => $pid ? API::helpers::trim(`ps -o lstart= -p $pid`) : undef,
+            etimes => $pid ? API::helpers::trim(`ps -o etimes= -p $pid`) : undef,
+        };
+        for my $group ( $config->find_groups({}) ) {
+            for ( $group->options ) {
+                $json->{data}{dhcp}{'groups'}{$group->{name}}{'options'}{$_->{name}} = $_->{value};
+            }
+            for my $host ( @{$group->hosts} ) {
+                my $mac = $host->_children->[0]->{value};
+                my ($lease) = grep { $_->hardware_address =~ /$mac/i } @{$leases->leases};
+                $json->{data}{dhcp}{'groups'}{$group->{name}}{'hosts'}{$host->{name}} = {
+                    name             => $host->{name},
+                    hardware_address => $host->_children->[0]->{value},
+                    vivso            => $host->options->[0]->{value},
+                };
+                $json->{data}{dhcp}{'groups'}{$group->{name}}{'hosts'}{$host->{name}}{'lease'} = {
+                   ip_address => $lease->ip_address, client_hostname => $lease->client_hostname,
+                   state => $lease->state, starts => $lease->starts, ends => $lease->ends,
+                } if( defined $lease );
+            }
+            $json->{data}{dhcp}{'groups'}{$group->{name}} = {} unless defined($json->{data}{dhcp}{'groups'}{$group->{name}});
+        }
+        for my $lease ( $leases->leases ) {
+            $json->{data}{dhcp}{'leases'}{$lease->ip_address}{'hardware_address'} = $lease->hardware_address;
+            $json->{data}{dhcp}{'leases'}{$lease->ip_address}{'client_hostname'}  = $lease->client_hostname;
+            $json->{data}{dhcp}{'leases'}{$lease->ip_address}{'starts'}           = $lease->starts;
+            $json->{data}{dhcp}{'leases'}{$lease->ip_address}{'state'}            = $lease->state;
+            $json->{data}{dhcp}{'leases'}{$lease->ip_address}{'ends'}             = $lease->ends;
+            
+            my $mac = $lease->hardware_address;
+            if( defined $mac ) {
+                for my $groupName (sort keys %{$json->{data}{dhcp}{groups}}) {
+                    if( defined $json->{data}{dhcp}{groups}{$groupName}{hosts} ) {
+                        my ($host) = grep { $json->{data}{dhcp}{groups}{$groupName}{hosts}{$_}->{hardware_address} =~ /$mac/i } keys %{$json->{data}{dhcp}{groups}{$groupName}{hosts}};
+                        if( $host ) {
+                            $json->{data}{dhcp}{'leases'}{$lease->ip_address}{'host'}             = {
+                                name  => $json->{data}{dhcp}{groups}{$groupName}{hosts}{$host}{name},
+                                vivso => $json->{data}{dhcp}{groups}{$groupName}{hosts}{$host}{vivso},
+                                group => $groupName,
+                            };
+                            last; 
+                        }
+                    }
+                }
+            }
+        }
+    };
     
     ### Check if subclass and requested function exists before initialize node and execute it.
     my ($reqPackage,$reqSubclass,$reqFunc) = ( $json->{meta}{postdata}{method} =~ /^(\w+)(?:\.(\w+))?(?:\.(\w+))?/ );
@@ -139,7 +130,7 @@ sub run {
                     $config,
                     $json->{meta}{postdata}{params} || undef
                 );
-                create_dhcp_hash($json,$config,$leases);
+                $create_dhcp_hash->($json,$config,$leases);
                 $write_dhcpd_conf->() unless($method_run_result);
                 return $method_run_result
                     unless( $isHtml );
@@ -148,7 +139,7 @@ sub run {
             return {'rc'=>400,'msg'=>"Requested function '".($reqFunc || '')."' does not exist in package '$reqPackage.$subclass' (class.subclass.function). Abort!"};
         }
     } elsif ( !$reqSubclass || $reqSubclass eq '' || $reqSubclass eq 'get' ) {
-        create_dhcp_hash($json,$config,$leases);
+        $create_dhcp_hash->($json,$config,$leases);
         return { 'method' => defined $reqSubclass && $reqSubclass eq 'get' ? $reqPackage.'.'.$reqSubclass : $reqPackage }
             unless( $isHtml );
     } else {
